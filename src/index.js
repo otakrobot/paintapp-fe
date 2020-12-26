98ecbf13d7e40625a1c1cac7175b20e1a162fa36
mhdjanuar <mhdjanuar.17@gmail.com>
2020-12-26 13:57:49 +0700

    updated make canvas

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { CanvasProvider } from "./CanvasContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CanvasProvider>
+      <App />
+    </CanvasProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
